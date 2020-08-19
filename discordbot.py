3339e8d4c871cf088dfe1b7a19d49ad0573a5a0d
&夏 <69879011+andsummer@users.noreply.github.com>
2020-08-19 12:12:53 +0900

    ｓｓｓ

--- discordbot.py
+++ discordbot.py
@@ -21,11 +21,12 @@ async def プリコネの年末年始は(ctx):
 @bot.command()
 async def kokka(ctx):
     await ctx.send('ヤバイわ代　作詞:キャル 作曲:Cygames　ヤバイわ代は パチンコ代に家賃代に　シンフォギアでデュランダル折り 北斗無双で金保留外れ貧困層となりて　天龍で捲ろうと残資金をつぎ込みやけくそになって餃子の王将で散財するまで')
-    
-    
- # 返信する非同期関数を定義
+
+     
+@bot.command()
+# 返信する非同期関数を定義
 async def reply(message):
-    reply = f'{message.author.mention} ぶっ殺すわよ！' # 返信メッセージの作成
+    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
     await message.channel.send(reply) # 返信メッセージを送信
 
 # 発言時に実行されるイベントハンドラを定義
